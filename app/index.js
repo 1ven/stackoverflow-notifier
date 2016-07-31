09a6bc6783862c0119d9d3e9a5c2770de2242be0
Ivan Mironchik <me.roaiven@gmail.com>
2016-07-31 18:50:07 +0300

    Install babel and react

---
+++ app/index.js
@@ -0,0 +1,10 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+function App() {
+  return (
+    <div>Test</div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
