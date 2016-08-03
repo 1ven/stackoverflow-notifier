4e7c28481cebe08ef4b4ee336d721144df1118cc
Ivan Mironchik <me.roaiven@gmail.com>
2016-08-03 20:17:42 +0300

    Handle FETCH_LAST_QUESTION_SUCCESS

--- app/sagas/index.js
+++ app/sagas/index.js
@@ -1,33 +1,57 @@
-import { takeEvery } from 'redux-saga';
+import { takeEvery, delay } from 'redux-saga';
 import { call, select, put } from 'redux-saga/effects'
-import { fetchLastQuestion } from '../actions';
+import { fetchLastQuestion, addQuestion } from '../actions';
 import api from '../services/api';
 
 function* fetchLastQuestionTask(action) {
+  const { tag } = action.payload;
   try {
     const response = yield call(api.fetchLastQuestion, action.payload.tag);
-    const derivedQuestion = response.result.items[0];
-    const questions = yield select(state => state.questions);
-    const isQuestionFresh = questions.filter(q => q.question_id === derivedQuestion.question_id).length === 0;
 
-    if (isQuestionFresh) {
-      yield put(fetchLastQuestion.success({
-        question: derivedQuestion,
-      }));
-    }
+    yield put(fetchLastQuestion.success({
+      question: {
+        data: response.result.items[0],
+        tag,
+      },
+    }));
   } catch(err) {
     yield put(fetchLastQuestion.failure(err.message));
   }
 }
 
+function* handleFetchLastQuestionSuccess(action) {
+  const { question } = action.payload;
+  const questions = yield select(state => state.questions);
+  const isQuestionFresh = questions.filter(q => q.data.question_id === question.data.question_id).length === 0;
+  const shouldDisplayNotification = questions.filter(q => q.tag === question.tag).length;
+
+  if (isQuestionFresh) {
+    yield put(addQuestion(question));
+  }
+
+  if (isQuestionFresh && shouldDisplayNotification) {
+    new Notification('New question!', {
+      body: question.data.title,
+    });
+  }
+}
+
 function* startListenQuestionsTask(action) {
-  for (let tag of action.payload.tags) {
-    yield put(fetchLastQuestion.request({ tag }));
+  while (true) {
+    for (let tag of action.payload.tags) {
+      yield put(fetchLastQuestion.request({ tag }));
+    }
+
+    yield call(delay, 6000);
   }
 };
 
 function* watchFetchLastQuestion() {
   yield* takeEvery('LAST_QUESTION_FETCH_REQUEST', fetchLastQuestionTask);
+}
+
+function* watchFetchLastQuestionSuccess() {
+  yield* takeEvery('LAST_QUESTION_FETCH_SUCCESS', handleFetchLastQuestionSuccess);
 }
 
 function* watchStartListenQuestions() {
@@ -38,5 +62,6 @@ export default function* rootSaga() {
   yield [
     watchStartListenQuestions(),
     watchFetchLastQuestion(),
+    watchFetchLastQuestionSuccess(),
   ];
 };
