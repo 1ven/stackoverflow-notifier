685c7b0fa620db7ff5aa05d3bf30fe99dbb96347
Ivan Mironchik <me.roaiven@gmail.com>
2016-08-03 19:01:38 +0300

    Change window width

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 500})
+  mainWindow = new BrowserWindow({width: 900, height: 500})
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
