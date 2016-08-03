77bbb0e17882fc8cd64fb9d2e62380cb40d3e781
Ivan Mironchik <me.roaiven@gmail.com>
2016-08-03 18:59:33 +0300

    Install logger

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,15 +1,18 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from '../sagas';
+import createLogger from 'redux-logger';
 
 export default function configureStore(initialState) {
   const sagaMiddleware = createSagaMiddleware();
+  const loggerMiddleware = createLogger();
 
   const store = createStore(
     state => ({}),
     initialState,
     applyMiddleware(
-      sagaMiddleware
+      sagaMiddleware,
+      loggerMiddleware
     )
   );
 
