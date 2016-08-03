de52642698b6c0b00895bb2001251b9248aeaccb
Ivan Mironchik <me.roaiven@gmail.com>
2016-08-03 18:53:55 +0300

    Replace functional by class component

--- app/index.js
+++ app/index.js
@@ -1,21 +1,50 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-
 import 'babel-polyfill';
 
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import configureStore from './store/configureStore';
 
 const store = configureStore();
 
-store.dispatch({
-  type: 'START_LISTEN_QUESTIONS',
-  tags: ['reactjs', 'redux']
-});
+class App extends Component {
+  constructor() {
+    super();
 
-function App() {
-  return (
-    <div>Test</div>
-  );
+    this.state = {
+      questions: [],
+    };
+  }
+
+  componentWillMount() {
+    this.unsubscribe = store.subscribe(() => {
+      this.setState({
+        questions: store.getState().questions,
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  componentDidMount() {
+    store.dispatch({
+      type: 'START_LISTEN_QUESTIONS',
+      tags: ['reactjs', 'redux'],
+    });
+  }
+
+  render() {
+    const { questions } = this.state;
+
+    return questions.length ? (
+      questions.map((q, i) =>
+        <div>{q.title}</div>
+      )
+    ) : (
+      <div>Questions not found.</div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
